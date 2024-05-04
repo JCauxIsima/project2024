09b3c1f0fa5f506327b8ac3d939c45a976231f15
Jonathan Caux <jcaux@adobe.com>
2024-05-04 19:39:53 +0200

    Add missing ressource initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 #include <FenetrePrincipale/FenetrePrincipale.hpp>
 
 #include <QApplication>
+#include <QDir>
 
 int main(int argc, char* argv[])
 {
   QApplication app(argc, argv);
+
+	Q_INIT_RESOURCE(resource);
 
   TestCompil::FenetrePrincipale::MainWindow fenetre;
   fenetre.show();
